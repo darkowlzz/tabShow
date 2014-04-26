2d01218d20b0d84c82d3d1ed8113680abf1ab0c0
sunny <indiasuny000@gmail.com>
2014-04-26 23:17:36 +0530

    Using WindowMediator to deal with tabs and updated tab title font.

--- data/frame.js
+++ data/frame.js
@@ -1,7 +1,6 @@
 window.addEventListener('message', updateTabTitle, false);
 
 function updateTabTitle(message) {
-  console.log('data received: ' + message.data);
   var title = document.getElementById('title');
   title.textContent = message.data;
 }
